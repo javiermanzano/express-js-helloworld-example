6ad46a7a46f94b95b631c5f6622a651d3aa28261
Javier Manzano <javier.manzano22@gmail.com>
2019-12-20 14:07:16 +0100

    deploy

--- index.js
+++ index.js
@@ -6,7 +6,5 @@ app.get('/', function(req, res){
   res.json({ hello: 'Soamee rules!', env: 'production' });
 });
 
-if (!module.parent) {
-  app.listen(3000);
-  console.log('Express started on port 3000');
-}
+app.listen(3000);
+console.log('Express started on port 3000');
